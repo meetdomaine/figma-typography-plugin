15e1e3966dfeaeffe330d8428357bafc164967d9
Cody Crandall <cody@halfhelix.com>
2022-11-15 15:42:17 -0600

    initial commit

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -4,16 +4,17 @@ import '../styles/ui.css';
 declare function require(path: string): any;
 
 const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
-
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
+    const [outputType, setOutputType] = React.useState('tailwind');
+    const [outputUnits, setOutputUnits] = React.useState('rem');
+    const [output, setOutput] = React.useState('');
 
     const onCreate = () => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+        parent.postMessage(
+            {
+                pluginMessage: {type: 'output-typography', outputType, outputUnits},
+            },
+            '*'
+        );
     };
 
     const onCancel = () => {
@@ -24,23 +25,41 @@ const App = ({}) => {
         // This is how we read messages sent from the plugin controller
         window.onmessage = (event) => {
             const {type, message} = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            }
+            console.log({type, message}, event.data);
         };
     }, []);
 
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
+            <h2>Typography boilerplate</h2>
             <p>
-                Count: <input ref={countRef} />
+                This plugin is intended to minimize the time developers spend setting up fonts for development, and
+                reduce the back and forth required between design and developers.
             </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
+            <p>Getting Started</p>
+            <ol>
+                <li>Save some text styles in your Figma project. (Most projects should already have these)</li>
+                <li>select your output format (Currently only supports tailwind but native css support is coming)</li>
+                <li>select output units (px or rem)</li>
+                <li>click Create</li>
+            </ol>
+            <span>Select output (CSS/Tailwind)</span>
+            <select name="output" id="output" disabled>
+                <option value="tailwind">Tailwind</option>
+                <option value="css">CSS</option>
+            </select>
+
+            <span> output units (px, rem): </span>
+            <select name="units" id="units" onChange={({target: {value}}) => setOutputUnits(value)}>
+                <option value="rem">rem</option>
+                <option value="px">pixels</option>
+            </select>
+            <span>
+                <button onClick={onCreate}>Create</button>
+                <button onClick={onCancel}>Cancel</button>
+            </span>
+
+            <textarea>{output}</textarea>
         </div>
     );
 };
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,136 @@
-figma.showUI(__html__);
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
-figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
 
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, {width: 1000, height: 1000});
 
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+const removeLeadingTrailingCharacters = (str) => {
+    const specialCharacters = ['-', '_', '/', ' '];
+    if (specialCharacters.includes(str[0])) {
+        return str.substring(1);
+    }
+    if (specialCharacters.includes(str[str.length - 1])) {
+        return str.substring(0, str.length - 1);
+    }
+    return str.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase();
+};
 
-        // This is how figma responds back to the ui
-        figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
-        });
+const getBaseName = (name) =>
+    name
+        .replace(/[^a-zA-Z0-9]/g, '-')
+        .replace(/mobile|desktop/gi, '')
+        .toLowerCase();
+
+const getFontStyles = (font, isDesktop = false, otherViewportFont = null) => {
+    const breakpointModifier = isDesktop ? 'sm:' : '';
+    const name = `${breakpointModifier}text-${font.name.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase()}`;
+
+    const shouldShowValue = (value, otherValue) => !isDesktop || value !== otherValue;
+
+    const fontWeight = shouldShowValue(font.fontName.style, otherViewportFont?.fontName?.style)
+        ? `${breakpointModifier}font-${font.fontName.style.toLowerCase()}`
+        : '';
+
+    const fontFamily = shouldShowValue(font.fontName.family, otherViewportFont?.fontName?.family)
+        ? `${breakpointModifier}font-${getBaseName(font.fontName.family)}`
+        : '';
+
+    return `${name} ${fontWeight} ${fontFamily}`;
+};
+
+figma.ui.onmessage = async (msg) => {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === 'cancel') {
+        figma.closePlugin();
     }
 
-    figma.closePlugin();
+    if (msg.type === 'output-typography') {
+        await figma.loadFontAsync({family: 'Inter', style: 'Regular'});
+
+        const savedTextStyles = figma.getLocalTextStyles();
+        const {outputType, outputUnits} = msg;
+
+        const mapTailwindConfig = () => {
+            const result = savedTextStyles.map(({name, fontSize, letterSpacing, lineHeight}) => {
+                const cleanedName = name.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase();
+
+                const convertToCurrentUnits = (fontStyle) => {
+                    let value;
+                    if (typeof fontStyle === 'number') {
+                        value = fontStyle;
+                    } else if (fontStyle.unit === 'PERCENT') {
+                        value = round((fontStyle.value / 100) * fontSize);
+                    } else if (fontStyle.unit === 'PIXELS') {
+                        value = fontStyle.value;
+                    }
+
+                    return (outputUnits === 'rem' ? round(value / 16) : value) + outputUnits;
+                };
+
+                return {
+                    [cleanedName]: [
+                        convertToCurrentUnits(fontSize),
+                        {
+                            letterSpacing: convertToCurrentUnits(letterSpacing),
+                            lineHeight: convertToCurrentUnits(lineHeight),
+                        },
+                    ],
+                };
+            });
+
+            return {theme: {fontSize: result}};
+        };
+
+        const mapTypographyConfig = () => {
+            const mobileFonts = savedTextStyles.filter((style) => style.name.toLowerCase().includes('mobile'));
+
+            const result = mobileFonts.map((font) => {
+                const matchingDesktopFont = savedTextStyles.find(({name}) =>
+                    getBaseName(font.name).includes(getBaseName(name))
+                );
+
+                const getTitle = (font) => removeLeadingTrailingCharacters(getBaseName(font.name));
+
+                return `.${getTitle(font)} {@apply ${getFontStyles(font)} ${getFontStyles(
+                    matchingDesktopFont,
+                    true,
+                    font
+                )}}`;
+            });
+
+            return result.join('<br/>');
+        };
+
+        const mapCSSOutput = () => {};
+
+        const result =
+            outputType === 'tailwind'
+                ? {
+                      config: {'tailwind.config.js': mapTailwindConfig()},
+                      css: {'typography.css': mapTypographyConfig()},
+                  }
+                : mapCSSOutput();
+
+        figma.ui.postMessage(result);
+        // figma.showUI(
+        //   __html__ +
+        //     JSON.stringify(result?.config, null, "\t") +
+        //     "<br/><br/><br/>" +
+        //     JSON.stringify(result?.css, null, "<br/>"),
+        //   {
+        //     width: 1000,
+        //     height: 1000,
+        //   }
+        // );
+    }
 };
+
+const round = (x: any) => parseFloat(x.toFixed(2));
