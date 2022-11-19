7b1d6e38389bee7732180742e0a00e3731a297f7
Cody Crandall <cody@halfhelix.com>
2022-11-19 15:46:29 -0600

    Redesigns UI, adds ability to hide colors

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -7,25 +7,26 @@ declare function require(path: string): any;
 const App = ({}) => {
     const [outputType, setOutputType] = React.useState('tailwind');
     const [outputUnits, setOutputUnits] = React.useState('rem');
+    const [showColors, setShowColors] = React.useState(true);
     const [output, setOutput] = React.useState('');
     const [showTailwindConfig, setShowTailwindConfig] = React.useState(true);
 
     const handleCreate = () => {
         parent.postMessage(
             {
-                pluginMessage: {type: 'output-typography', outputType, outputUnits},
+                pluginMessage: {type: 'output-typography', outputType, outputUnits, showColors},
             },
             '*'
         );
     };
 
-    const onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
+    const handleClose = () => {
+        parent.postMessage({pluginMessage: {type: 'close'}}, '*');
     };
 
     React.useEffect(() => {
         handleCreate();
-    }, [outputType, outputUnits]);
+    }, [outputType, outputUnits, showColors]);
 
     React.useEffect(() => {
         // This is how we read messages sent from the plugin controller
@@ -42,29 +43,61 @@ const App = ({}) => {
         <div className="main">
             <div className="main-wrapper">
                 <nav>
+                    <button className="close" type="button" onClick={handleClose}>
+                        <svg
+                            width="6"
+                            height="6"
+                            viewBox="0 0 26 26"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="close-cross"
+                        >
+                            <path
+                                d="M25 1L1.33997 24.66L25 1Z"
+                                stroke="black"
+                                strokeMiterlimit="10"
+                                strokeLinecap="round"
+                            />
+                            <path
+                                d="M24.66 24.66L1 1L24.66 24.66Z"
+                                stroke="black"
+                                strokeMiterlimit="10"
+                                strokeLinecap="round"
+                            />
+                        </svg>
+                    </button>
                     <button
                         type="button"
                         onClick={() => setShowTailwindConfig(true)}
-                        className={`file-name ${showTailwindConfig ? 'selected' : ''}`}
+                        className={`tab ${showTailwindConfig ? 'selected' : ''}`}
                     >
                         <p>tailwind.config.js</p>
                     </button>
                     <button
                         type="button"
                         onClick={() => setShowTailwindConfig(false)}
-                        className={`file-name ${!showTailwindConfig ? 'selected' : ''}`}
+                        className={`tab ${!showTailwindConfig ? 'selected' : ''}`}
                     >
                         <p>typography.css</p>
                     </button>
-                    <CopyIcon value={showTailwindConfig ? output.config : output.css} />
-                    <button type="button" onClick={() => setOutputUnits(outputUnits === 'rem' ? 'px' : 'rem')}>
-                        Toggle output to {outputUnits === 'rem' ? 'pixels' : 'rem'}
-                    </button>
-                    <button onClick={onCancel} className="close">
-                        Close
-                    </button>
                 </nav>
-                <textarea spellCheck={false} value={showTailwindConfig ? output.config : output.css} readOnly />
+                <div className="output">
+                    <div className="toggles-wrapper">
+                        <CopyIcon value={showTailwindConfig ? output.config : output.css} />
+
+                        <button
+                            className="toggle"
+                            type="button"
+                            onClick={() => setOutputUnits(outputUnits === 'rem' ? 'px' : 'rem')}
+                        >
+                            View in {outputUnits === 'rem' ? 'pixels' : 'rem'}
+                        </button>
+                        <button className="toggle" type="button" onClick={() => setShowColors(!showColors)}>
+                            {showColors ? 'Hide' : 'Show'} colors
+                        </button>
+                    </div>
+                    <textarea spellCheck={false} value={showTailwindConfig ? output.config : output.css} readOnly />
+                </div>
             </div>
         </div>
     );
