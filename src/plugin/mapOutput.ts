d12112b44c5b0616843f7e15a580a44a08aedd4a
Cody Crandall <58891378+codyscott1@users.noreply.github.com>
2022-11-18 23:03:09 -0600

    Output casing values to typography.css (#5)

--- src/plugin/mapOutput.ts
+++ src/plugin/mapOutput.ts
@@ -20,11 +20,12 @@ const getBaseName = (name) =>
 const round = (x) => parseFloat(x.toFixed(2));
 
 const getFontStyles = (font, isDesktop, currentStyles) => {
+    console.log(font);
     const breakpointModifier = isDesktop ? 'sm:' : '';
     const shouldShowValue = (value) => !!value && (!isDesktop || !currentStyles.includes(value.toLowerCase()));
 
-    console.log(currentStyles);
     const styles = font.fontName.style.split(' ');
+    const {textCase} = font;
     const fontWeight = styles[0];
     const fontStyle = styles[1] || '';
 
@@ -34,8 +35,17 @@ const getFontStyles = (font, isDesktop, currentStyles) => {
     const parsedFamily = shouldShowValue(font.fontName.family)
         ? ` ${breakpointModifier}font-${getBaseName(font.fontName.family)}`
         : '';
-    console.log({parsedName, parsedWeight, parsedStyle, parsedFamily});
-    return currentStyles + parsedName + parsedWeight + parsedStyle + parsedFamily;
+    const uppercase = textCase === 'UPPER' && shouldShowValue('uppercase') ? ` ${breakpointModifier}uppercase` : '';
+
+    const lowercase = textCase === 'LOWER' && shouldShowValue('lowercase') ? ` ${breakpointModifier}lowercase` : '';
+
+    const originalCase =
+        textCase === 'ORIGINAL' && (!shouldShowValue('uppercase') || !shouldShowValue('lowercase'))
+            ? ` ${breakpointModifier}normal-case`
+            : '';
+    return (
+        currentStyles + parsedName + parsedWeight + parsedStyle + parsedFamily + uppercase + lowercase + originalCase
+    );
 };
 
 export const mapTailwindConfig = (outputUnits) => {
