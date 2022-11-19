9d0b36626e671dc750be155712f69a025f324364
Cody Crandall <58891378+codyscott1@users.noreply.github.com>
2022-11-18 23:03:54 -0600

    Adds some error handling (#6)

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -8,16 +8,28 @@ figma.ui.onmessage = async ({type, outputType, outputUnits}) => {
     }
 
     if (type === 'output-typography') {
+        if (!figma.getLocalTextStyles()?.length) {
+            figma.notify('No saved text styles found. Please ensure text styles are saved');
+        }
+
         await figma.loadFontAsync({family: 'Inter', style: 'Regular'});
-
-        const result =
-            outputType === 'tailwind'
-                ? {
-                      config: mapTailwindConfig(outputUnits),
-                      css: mapTypographyConfig(),
-                  }
-                : {};
-
-        figma.ui.postMessage(result);
+        try {
+            const result =
+                outputType === 'tailwind'
+                    ? {
+                          config: mapTailwindConfig(outputUnits),
+                          css: mapTypographyConfig(),
+                      }
+                    : {};
+            figma.ui.postMessage(result);
+        } catch (error) {
+            figma.notify(
+                'Whoops! Something went wrong. Please try again or check the console for more details. Ensure text styles are saved'
+            );
+            console.error(
+                "Your fonts could not be generated! Please ensure you have saved text styles. If you're still having issues, please reach out by creating a new issue at https://github.com/codyscott1/figma-typography-plugin/issues . If you could be so kind as to include the error from the console it would be greatly appreciated."
+            );
+            console.error(JSON.stringify(error));
+        }
     }
 };
